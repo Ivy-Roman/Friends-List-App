54fc8eecc5145676ce60bba7dd1d97fbb090b5f1
Ivie Osoiye <ivieosoiye@gmail.com>
2023-02-13 15:42:51 +0100

    added passeord field to the Meta class

--- friendslist_api/authentication/serializers.py
+++ friendslist_api/authentication/serializers.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializers):
+class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         max_length=65, min_length=8,write_only=True)
     email = serializers.EmailField(max_length=255, min_length=4)
@@ -10,13 +10,14 @@ class UserSerializer(serializers.ModelSerializers):
 
     class Meta:
         model = User
-        fields=['username', 'firstname', 'lastname', 'email']
+        fields=['username', 'first_name', 'last_name', 'email', 'password']
 
     def validate(self, attrs):
-        if User.objects.filter(email=attrs['emails']).exists():
+        email = attrs.get('email', '')
+        if User.objects.filter(email=email).exists():
             raise serializers.ValidationError(
-                {'email', ('Email is already in use')})
+                {'email': ('Email is already in use')})
         return super().validate(attrs)
 
     def create(self, validated_data):
-        return User.objects.create_user(validated_data)
+        return User.objects.create_user(**validated_data)
